7e8ffa61c509e6af18c160d2a5e4f7bb10445ee5
irina <user@irina-comp.forma>
2020-06-12 17:10:11 +0300

    Add remove by click and another changes

--- js/background.js
+++ js/background.js
@@ -1,7 +1,30 @@
-chrome.browserAction.onClicked.addListener(function () {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "printPage"}, function(response) {
-
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'printPage' }, function(response) {
     });
   });
+
+chrome.contextMenus.create({"title": "PagePrinter", "id": "parent", "contexts":["page"],
+  "onclick": genericOnClick
 });
+
+chrome.contextMenus.create(
+    {"title": "printPage", "parentId": "parent", "id": "child1"});
+chrome.contextMenus.create(
+    {"title": "Somithing else", "parentId": "parent", "id": "child2"});
+
+function genericOnClick() {
+  alert('Hello');
+}
+
+chrome.contextMenus.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript(
+    tab.id,
+    {code: 'window.print();'});
+});
+
+
+
+
+
+
--- js/content.js
+++ js/content.js
@@ -1,48 +1,108 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender) {
-    if(request.action === "printPage") {
-      openPreviewModal();
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    const { action } = request;
+    if (action === 'printPage') {
+      return openPreviewModal();
     }
+});
+
+function openPreviewModal() {
+  const htmlPage = `
+    <html>
+      ${document.documentElement.innerHTML}
+    </html>`;
+  const html = `
+    <div id="pagePrinter" class="modal tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-lg"" role="document" style="height: 100%">
+        <div class="modal-content" style="height: 100%">
+          <div class="modal-header">
+            <button id="printBtn" class=btn btn-primary">print</button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">Modal title</h4>
+          </div>
+          <div class="modal-body" style="height: 100%">
+            <!--- <iframe id="iframeId" class="pp" height="100%" width="100%"/> -->
+            <iframe id="iframeId" height="100%" width="100%" />
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div>
+  `;
+
+  let div = document.getElementById('extContent');
+  if(!div) {
+    const div = document.createElement('div');
+    div.classList.add('no-print', 'pp');
+    div.id = 'extContent';
+    document.body.appendChild(div);
+    div.innerHTML = html;
+  }
+
+  $('#pagePrinter').modal('toggle');
+
+  const pagePrinterIframe = document.getElementById('iframeId');
+  pagePrinterIframe.contentDocument.open();
+  pagePrinterIframe.contentDocument.write(htmlPage);
+  pagePrinterIframe.contentDocument.close();
+
+  $('#iframeId').on('load', () => {
+    const link  = pagePrinterIframe.contentDocument.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = chrome.runtime.getURL('css/iframe.css');
+    link.media = 'all';
+    const head = pagePrinterIframe.contentDocument.getElementsByTagName('head')[0];
+    head.appendChild(link);
+    const a = $('#iframeId').contents().find('a');
+    a.click(function(e){e.preventDefault()});
+    const elements = $('#iframeId').contents().find(`
+      div,
+      span,
+      p,
+      a,
+      table,
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6,
+      input,
+      li,
+      ul,
+      button,
+      select,
+      header,
+      img,
+      article,
+      footer,
+      form,
+      label,
+      svg,
+      rect,
+      nav
+    `);
+
+    elements.mouseover(function(event) {
+      console.log(elements);
+      event.stopPropagation();
+      $('#iframeId').contents().find('.pagePrinter--over').removeClass('pagePrinter--over');
+      $(this).addClass('pagePrinter--over');
+      $(this).on('click', function(event) {
+        event.stopPropagation();
+   //     $(this).addClass('pagePrinter--hidden');
+   console.log($(this));
+      $(this).remove();
+      });
+    });
   });
 
-  function openPreviewModal() {
-    const pageHtml = `<html> ${document.documentElement.innerHTML}</html>`;
-    const modalHtml =
-      `<div id="pagePrinter" class="modal hidden-print" tabindex="-1" role="dialog">
-        <div class="modal-dialog.modal-lg modal-dialog-scrollable fullSize">
-          <div class="modal-content fullSize">
-            <div class="modal-header">
-              <button type="button" id="openPrinterPage" class="btn btn-primary btn-sm">Print</button>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body fullSize">
-              <iframe id="pagePrinterIframe" width="100%" height="100%" allowfullscreen />
-            </div>
-          </div>
-        </div>
-      </div>`;
+  $('#printBtn').on('click', function () {
+    printFrame('iframeId');
+  });
+}
 
-    let div = document.getElementById('extContent');
-
-    if(!div) {
-      div = document.createElement('div');
-      div.id = 'extContent';
-      div.innerHTML = modalHtml;
-      document.body.appendChild(div);
-    }
-
-    const pagePrinterIframe = document.getElementById('pagePrinterIframe')
-
-    pagePrinterIframe.contentDocument.open();
-    pagePrinterIframe.contentDocument.write(pageHtml);
-    pagePrinterIframe.contentDocument.close();
-
-  // works in the most cases. but sometimes there are errors with this attribute
-  //  pagePrinterIframe.srcdoc = pageHtml;
-    $('#pagePrinter').modal('toggle');
-    $('#openPrinterPage').on('click', function() {
-      window.print();
-    });
-  }
+function printFrame(id) {
+  var frm = document.getElementById(id).contentWindow;
+  frm.print();
+  return false;
+}
